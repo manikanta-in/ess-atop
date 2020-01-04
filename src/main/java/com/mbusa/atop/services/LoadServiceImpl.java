53cb868c37d64b50d0c06e0fb1bcd6e0fc7c28c6
Manikanta Kuchipudi <mkuchipudi@modeln.com>
2020-01-04 21:04:04 +0530

    changes

--- src/main/java/com/mbusa/atop/services/LoadServiceImpl.java
+++ src/main/java/com/mbusa/atop/services/LoadServiceImpl.java
@@ -3,10 +3,12 @@ package com.mbusa.atop.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mbusa.atop.db.model.Load;
 import com.mbusa.atop.repo.LoadRepo;
 
+@Service
 public class LoadServiceImpl implements LoadService {
 	
 	@Autowired
